542ee297bb05645685267afeb744ec5ec3f130c8
dickwyn <dickwyn@outlook.com>
2015-11-12 23:43:36 +0800

    added code reuse script for all pages

---
+++ site/scripts/js/elementReuse.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.header').load("reusable/header.html");
+    $('.footer').load("reusable/footer.html");
+    $('.subjectListing').load("reusable/subjectListing.html")
+});
