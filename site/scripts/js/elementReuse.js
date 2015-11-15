1dbef7df4496b217b10de812e7a2f94041224fe8
dickwyn <dickwyn@outlook.com>
2015-11-16 01:29:24 +0800

    updated image assets

--- site/scripts/js/elementReuse.js
+++ site/scripts/js/elementReuse.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.header').load("reusable/header.html");
+    $('.header').load("reusable/navbar.html");
     $('.footer').load("reusable/footer.html");
     $('.subjectListing').load("reusable/subjectListing.html")
 });
